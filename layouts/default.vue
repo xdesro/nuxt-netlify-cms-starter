8277055102ff3a657e5fbb47541348806367363c
Jeffrey Ernst <ernst.j.c@gmail.com>
2020-01-10 01:39:49 +0100

    Moved general styling from the main index PAGE to Default LAYOUT for styling to maintain on subpath

--- layouts/default.vue
+++ layouts/default.vue
@@ -17,6 +17,39 @@ html {
   box-sizing: border-box;
 }
 
+a {
+  font-weight: bold;
+  color: #526488;
+}
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: bold;
+  font-size: 3.2rem;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 2rem;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+
 *,
 *:before,
 *:after {
--- pages/index.vue
+++ pages/index.vue
@@ -33,37 +33,7 @@ export default {
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-    'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: bold;
-  font-size: 3.2rem;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 2rem;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-a {
-  font-weight: bold;
-  color: #526488;
-}
+<style scoped>
 .deploy-button {
   margin-top: 1rem;
 }
